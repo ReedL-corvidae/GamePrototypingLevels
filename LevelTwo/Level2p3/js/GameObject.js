efd9a206b33b0feab6a55e5733813ad676eace48
ReedL-corvidae <rclinehan@email.neit.edu>
2026-04-02 09:23:33 -0400

    its stuck now

--- LevelTwo/Level2p3/js/GameObject.js
+++ LevelTwo/Level2p3/js/GameObject.js
@@ -61,5 +61,37 @@ function GameObject(x,y,w,h,color)
 	{
 		this.x += this.vx;
 		this.y += this.vy;
+
+		this.left = function() 
+	{
+		return this.x - this.width/2;
+	}
+	this.right = function() 
+	{
+		return this.x + this.width/2;
+	}
+	
+	this.top = function() 
+	{
+		return this.y - this.height/2;
+	}
+	this.bottom = function() 
+	{
+		return this.y + this.height/2;
+	}
+	
+	//Eventually sets the hitboxes....
+	this.hitTestObject = function(obj)
+	{
+		if(this.left() < obj.right() && 
+		   this.right() > obj.left() &&
+		   this.top() < obj.bottom() &&
+		   this.bottom() > obj.top())
+		{
+			return true
+		}
+		return false;
+	}
+
 	}
 }
---
+++ LevelTwo/Level2p3/js/game.js
@@ -0,0 +1,71 @@
+var canvas;
+var context;
+var timer;
+var interval = 1000/60;
+var ball;
+var player1;
+
+	canvas = document.getElementById("canvas");
+	context = canvas.getContext("2d");
+
+    var player1 = new GameObject();	
+    var ball = new GameObject();
+	
+	//Declare the ball's speed on the x and y axis
+	ball.vx = -10;
+	ball.vy = 10;
+
+	var prevY;
+
+	timer = setInterval(animate, interval);
+
+function animate()
+{
+	context.clearRect(0,0,canvas.width, canvas.height);	
+	
+	//Movement using ball's move() function
+	ball.move();
+	
+	
+	//Bouncing the ball. Its ready.
+	if(ball.x > canvas.width - ball.width/2)
+	{
+		ball.vx = -ball.vx;	
+	}
+
+	if(ball.x < ball.width/2){
+		ball.vx *= -1;
+	}
+
+	if(ball.y > canvas.height - ball.height/2)
+	{
+		ball.vy = -ball.vy;	
+	}
+
+	if(ball.y < ball.height/2){
+		ball.vy *= -1;
+	}
+
+	//Moves player up and down
+	if(w)
+	{
+		console.log("Moving Up");
+		player1.y += -5;
+	}
+	if(s)
+	{
+		console.log("Moving Down");
+		player1.y += 5;
+	}
+
+	if((player1.y > canvas.height - player1.height) || (player1.y < player1.height/2))
+	{
+		player1.y = prevY;	
+	}
+	else
+	{
+		prevY = player1.y
+	}
+	
+    player1.drawRect();
+}
