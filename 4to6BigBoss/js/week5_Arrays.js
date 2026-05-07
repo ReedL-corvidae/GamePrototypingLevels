f5e25645c65a2015da4e3efa3e5d4e9cb0415b4d
ReedL-corvidae <rclinehan@email.neit.edu>
2026-05-07 08:37:07 -0400

    updates

--- 4to6BigBoss/js/week5_Arrays.js
+++ 4to6BigBoss/js/week5_Arrays.js
@@ -19,6 +19,7 @@ colors[2] = "#0000ff";
 
 var score = 0;
 var total = 5;	
+
 var dots = [];
 var squares = [];
 
@@ -28,9 +29,9 @@ var frictionY = .99;
 var player = new GameObject();
 	player.width = 50;
 	player.height = 50;
-	player.y = canvas.height - player.height - 20;
+	player.y = canvas.height - player.height - 25;
 	player.x = canvas.width/2;
-	player.color = "#341ef8"
+	player.color = "#0000ff"
 
 for(var i = 0; i < total; i++)
 {
@@ -91,10 +92,15 @@ function showFriction()
 	player.x += player.vx;	
 }
 
+function colorChange()
+{
+	player.color = "#0000ff";
+}
+
 function animate()
 {
 	context.clearRect(0,0,canvas.width, canvas.height);	
-	
+
 	player.move();
 
 	showAcceleration();
@@ -113,6 +119,24 @@ function animate()
 		dots[i].vy = rand(4,8);
 		console.log("changing locations");
 	}
+
+	if(dots[i].hitTestObject(player))
+		{
+			console.log("collision");
+			for(var j = 0; j < total; j++)
+			{
+				dots[j].x = Math.random() * canvas.width;
+				dots[j].y = Math.random() * -500;
+				dots[j].vy = rand(4,8);
+
+				squares[j].x = Math.random() * canvas.width;
+				squares[j].y = Math.random() * -500;
+				squares[j].vy = rand(4,8);
+			}
+			score = 0;
+			player.color = "#ff0000";
+			setTimeout(colorChange, 500);
+		}
 	}
 
 	for(var i = 0; i < total; i++)
@@ -121,14 +145,28 @@ function animate()
 		squares[i].y--;
 		squares[i].move();
 	
-	if(squares[i].y > canvas.height)
-	{
-		squares[i].x = Math.random() * canvas.width;
-		squares[i].y = Math.random() * -500;
-		squares[i].vy = rand(4,8);
-		console.log("changing locations");
+		if(squares[i].y > canvas.height)
+		{
+			squares[i].x = Math.random() * canvas.width;
+			squares[i].y = Math.random() * -500;
+			
+			console.log("changing locations");
+		}
+		if(squares[i].hitTestObject(player))
+		{
+			console.log("collision");
+
+			squares[i].x = Math.random() * canvas.width;
+			squares[i].y = Math.random() * -500;
+			squares[i].vy = rand(4,8);
+			score++;
+
+			player.color = "#00ff00";
+			setTimeout(colorChange, 500);
+		}
 	}
-	}
+
+	
 
 	if(player.x > canvas.width - player.width/2)
 	{
@@ -144,6 +182,10 @@ function animate()
 
 
 	player.drawRect();
+
+	context.font = "30px Arial";
+	context.fillText("Score:", 25, 50);
+	context.fillText(score, 115, 51);
 
 }
 
